daa2d432d240a877bf0c38b1491ea9bf4cd9fbdd
Azizo_pu01 <Azizo_pu01@itstep.edu.az>
2023-05-29 09:04:18 +0400

    sg

---
+++ Ado net connection task/dbo.sp_ShowStudentsByGroupId.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE sp_ShowStudentsByGroupId
+@groupId INT
+AS
+BEGIN
+ SELECT S.Firstname , S.Lastname , G.[Name]
+ FROM Students AS S
+ INNER JOIN Groups AS G
+ ON G.Id=S.Id_Group
+ WHERE G.Id=@groupId
+ 
+END
+
+CREATE PROCEDURE sp_AuthorsDelete
+@id AS INT
+AS
+BEGIN
+  DELETE FROM Authors
+  WHERE Id=@id
+
+END
